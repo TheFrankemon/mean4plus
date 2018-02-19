f8c54e8a274e14766266ddb1e501bcf846689209
TheFrankemon <francomontiel@hotmail.com>
2018-02-19 09:58:26 -0400

    Fixed date characters missing

--- src/app/incoming.component.ts
+++ src/app/incoming.component.ts
@@ -80,12 +80,12 @@ export class IncomingComponent {
 	getDate() : string {
 		//09/01/18 10:12:23
 		var now = new Date();
-		var timestamp = now.getDay() + "/"
-					  + now.getMonth() + "/"
+		var timestamp = (now.getDate() < 10 ? "0" + now.getDate() : now.getDate()) + "/"
+					  + ((now.getMonth() + 1) < 10 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1)) + "/"
 					  + now.getFullYear().toString().substr(2) + " "
-					  + now.getHours() + ":"
-					  + now.getMinutes() + ":"
-					  + now.getSeconds();
+					  + (now.getHours() < 10 ? "0" + now.getHours() : now.getHours()) + ":"
+					  + (now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()) + ":"
+					  + (now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds());
 		return timestamp;
 	}
 
